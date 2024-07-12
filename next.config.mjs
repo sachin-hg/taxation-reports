bce3033861d723f0be83de281aa9e5606316178b
Sachin Agrawal <sachin@gmail.com>
2024-07-12 14:52:43 +0530

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb', // Set the desired size limit here
+    },
+  }
 };
 
 export default nextConfig;
